f5c522d53c8a0a886723b7405fa91418ac4d0e04
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-04-21 02:45:27 +0200

    Fix: Enhanse get data from form

--- js/addJob.js
+++ js/addJob.js
@@ -2,36 +2,37 @@ document.addEventListener('DOMContentLoaded', function() {
   let form = document.querySelector('form');
 
   form.addEventListener('submit', function(event) {
-      event.preventDefault(); 
-      let jobs = JSON.parse(localStorage.getItem('jobs')) || [];
 
-      const jobTitle = document.getElementById('jobTitle').value;
-      const jobStatus = document.querySelector('select[name="jobStatus"]').value;
-      const yearsOfExperience = document.querySelector('select[name="yearsOfExperience"]').value;
-      const jobSalary = document.getElementById('jobSalary').value;
-      const jobDescription = document.getElementById('jobDescription').value;
-      const jobRequirements = document.getElementById('jobRequirements').value;
-      const companyName = document.getElementById('companyName').value;
-      const companyAddress = document.getElementById('companyAddress').value;
-      const jobId = generateJobId(jobs);
+    //prevent Default actions
+    event.preventDefault(); 
 
-      let jobData = {
-          jobId : jobId,
-          jobTitle: jobTitle,
-          jobStatus: jobStatus,
-          yearsOfExperience: yearsOfExperience,
-          jobSalary: jobSalary,
-          jobDescription: jobDescription,
-          jobRequirements: jobRequirements,
-          companyName: companyName,
-          companyAddress: companyAddress
-      };
+    //get jobs object from local storage
+    let jobs = JSON.parse(localStorage.getItem('jobs')) || [];
 
-      jobs.push(jobData);
+    // get data from form
+    var formData = new FormData(form);
+    var data = {};
+    formData.forEach(function(value, key){
+        data[key] = value;
+    });
 
-      localStorage.setItem('jobs', JSON.stringify(jobs));
-      const message = document.querySelector("#message");
-      message.style.color="green";
+    //create an id for a job
+    const jobId = generateJobId(jobs);
+    data[jobId] = jobId;
+
+    //check over data
+    console.log(data);
+    
+    //push new job to jobs array
+    jobs.push(data);
+
+    //save jobs in local storage
+    localStorage.setItem('jobs', JSON.stringify(jobs));
+
+    //set message to confirm save 
+    const message = document.querySelector("#message");
+    
+    message.style.color="green";
       message.style.display="flex";
       form.reset();
       setTimeout(function() {
@@ -41,6 +42,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 });
+
+//create an id using unique key
 function generateJobId(jobs) {
     let maxId = 0;
     jobs.forEach(function(job) {
