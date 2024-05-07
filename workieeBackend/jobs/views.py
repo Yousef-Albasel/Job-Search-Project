4f3b887e2fb4aa1bf14027bbfa3a4c657fe10a3c
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-05-07 04:41:05 +0300

    create view to list all jobs in db

--- workieeBackend/jobs/views.py
+++ workieeBackend/jobs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse , HttpResponseRedirect
+from django.template import loader
+from .models import Jobs
+from django.urls import reverse
 # Create your views here.
+
+def index(request):
+  tmp= loader.get_template('index.html')
+  alljobs=Jobs.objects.all().values()
+  context = {
+        'jobs' : alljobs,
+    }
+  return HttpResponse(tmp.render(context,request))
