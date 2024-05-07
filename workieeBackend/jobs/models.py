049c5153e26dd525c4ea61d533fe4c42662dab11
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-05-07 04:36:15 +0300

    make table of jobs

--- workieeBackend/jobs/models.py
+++ workieeBackend/jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Jobs(models.Model):
+    jobtitle = models.CharField(max_length=255)
+    jobsalary = models.IntegerField(default=0)
+    jobstatus = models.CharField(max_length=255)
+    yearsofexperience = models.CharField(max_length=255)
+    companyname = models.CharField(max_length=255)
+    companyaddress = models.CharField(max_length=255)
+    jobdescription = models.TextField(default='')
+    jobrequirements = models.TextField(default='')
