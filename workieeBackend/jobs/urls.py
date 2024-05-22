a46505969bc50a0ef60a7be4a782867295c93d89
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-05-22 18:33:47 +0300

    FEAT:edit job

--- workieeBackend/jobs/urls.py
+++ workieeBackend/jobs/urls.py
@@ -3,10 +3,11 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-  path('',views.index,name='index'),
+  path('',views.Index,name='Index'),
   path('add/',views.add,name='add'),
   path('add/addrecord/',views.addrecord,name='addrecord'),
   path('delete/<int:id>',views.delete,name='delete'),
   path('edit/<int:id>',views.edit,name='edit'),
+  path('update/',views.update,name='update'),
 
 ]
