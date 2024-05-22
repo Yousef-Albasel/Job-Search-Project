e24a4433ba2ea3bba8c79e6240d7a3cfb3787d34
Yousef-Albasel <albaselofficial@gmail.com>
2024-05-22 16:02:16 +0300

    Fixed Tags and some edits

--- js/profile.js
+++ js/profile.js
@@ -16,3 +16,32 @@ document.addEventListener('DOMContentLoaded', () => {
         profileImage.src = `https://i.pravatar.cc/150?timestamp=${timestamp}`;
     });
 });
+
+const newSkillButton = document.querySelector(".profile-elements .skills i.fa-plus");
+const deleteSkillButton = document.querySelector(".profile-elements .skills .tag i.fa-times");
+const newSkillInput = document.querySelector(".profile-elements input");
+
+newSkillButton.addEventListener('click',() => {
+    newSkillInput.style.display = "flex";
+})
+
+const skillsContainer = document.querySelector(".profile-elements .skills");
+newSkillInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        let tag = document.createElement("span");
+        let deleteButt = document.createElement("i");
+        tag.setAttribute("class","tag");
+        deleteButt.setAttribute("class","fas fa-times");
+        tag.innerHTML = newSkillInput.value;
+        tag.appendChild(deleteButt);
+        skillsContainer.removeChild(newSkillButton);
+        skillsContainer.appendChild(tag);
+        skillsContainer.appendChild(newSkillButton);
+        newSkillInput.value = "";
+        newSkillInput.style.display ="none";
+        deleteButt.addEventListener('click', function() {
+            skillsContainer.removeChild(tag);
+        });
+    }
+});
+
